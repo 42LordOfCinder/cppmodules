9869ac8cfd175d9a0e77e40042c1669aa7ae7e82
Gauthier Massoni <gmassoni@student.42angouleme.fr>
2024-09-04 17:26:34 +0200

    fixed 00/00

--- 00/ex00/megaphone.cpp
+++ 00/ex00/megaphone.cpp
@@ -7,7 +7,7 @@ int	main(int argc, char **argv) {
 		for (int i = 1; argv[i]; i++)
 			for (int j = 0; argv[i][j]; j++)
 				std::cout << (char) toupper(argv[i][j]);
-		std::cout << '\n';
+		std::cout << std::endl;
 	}
 	return 0;
 }
